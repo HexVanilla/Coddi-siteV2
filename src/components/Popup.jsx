7d84aa5450d1bc0f397be8faf7d52a40718642d1
HexVanilla <hexvanilla@gmail.com>
2023-05-10 07:13:48 -0700

    popup text and link added

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -17,8 +17,28 @@ const Popup = () => {
             <button className="popup-close-btn" onClick={handleClose}>
               &times;
             </button>
-            <h2>Welcome to our website!</h2>
-            <button className="popup-cta-btn">Learn More</button>
+            <h4>
+              Coddi is developing a new service for knowledge management in the
+              diagnostic of condition monitoring techniques, using Open AI Chat
+              GPT-3. We are committed to providing a high-quality service and we
+              want to ensure that it meets the needs of our customers.
+            </h4>
+            <h4>
+              That's why we are conducting this survey to gather feedback and
+              identify other functions that customers would like to see in our
+              service. Your input is valuable and will help us to continue to
+              improve and innovate.
+            </h4>
+            <button
+              className="popup-cta-btn"
+              onClick={() =>
+                window.open(
+                  "https://forms.office.com/Pages/ResponsePage.aspx?id=WWZiz1oUp0yCXVzSHqmCJtTg8BHgoRNKg4opqFSG3zJUOVFXM1Q4UE5ERFlBQ05LWk9LREoyTjRRMS4u&embed=true"
+                )
+              }
+            >
+              Take the survey
+            </button>
           </div>
         </div>
       )}
