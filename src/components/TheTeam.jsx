2c0c355f31fb896f76d710b05bd203ba034a1120
HexVanilla <97599030+HexVanilla@users.noreply.github.com>
2024-07-24 20:36:15 -0400

    new team member added plus mobile format fixes

--- src/components/TheTeam.jsx
+++ src/components/TheTeam.jsx
@@ -1,80 +1,87 @@
-import React from "react";
-import "./TheTeam.css";
-import martin from "../img/martin.png";
-import patricio from "../img/patricio.png";
-import jourdan from "../img/jourdan.png";
-import warren from "../img/warren.png";
-import pedro from "../img/pedro.png";
-import paola from "../img/paola-01.png";
-import cristobal from "../img/cristobal.png";
-import useIntersectionObserver from "./useIntersectionObserver";
-import { useRef } from "react";
+import React from 'react'
+import './TheTeam.css'
+import diego from '../img/diego-01.png'
+import martin from '../img/martin.png'
+import patricio from '../img/patricio.png'
+import jourdan from '../img/jourdan.png'
+import warren from '../img/warren.png'
+import pedro from '../img/pedro.png'
+import paola from '../img/paola-01.png'
+import cristobal from '../img/cristobal.png'
+import useIntersectionObserver from './useIntersectionObserver'
+import { useRef } from 'react'
 
 const TheTeam = ({ theme, language }) => {
-  const sectionRef = useRef();
-  const isVisible = useIntersectionObserver(sectionRef, { threshold: 0.1 });
+  const sectionRef = useRef()
+  const isVisible = useIntersectionObserver(sectionRef, { threshold: 0.1 })
   const teamMembers = [
     {
-      name: "Martin Lara Sepúlveda",
-      title: "Back-End Developer",
-      description: "martin@coddi.ai",
+      name: 'Diego Lagos Béltran',
+      title: 'Front-End Developer',
+      description: 'diego@coddi.ai',
+      image: diego,
+    },
+    {
+      name: 'Martin Lara Sepúlveda',
+      title: 'Back-End Developer',
+      description: 'martin@coddi.ai',
       image: martin,
     },
     {
-      name: "Patricio Ortiz Vargas, MSC.",
-      title: "Data Science",
-      description: "patricio@coddi.ai",
+      name: 'Patricio Ortiz Vargas, MSC.',
+      title: 'Data Science',
+      description: 'patricio@coddi.ai',
       image: patricio,
     },
     {
-      name: "Warren Eaton Sciaccaluga",
-      title: "Cloud Architect",
-      description: "warren@coddi.ai",
+      name: 'Warren Eaton Sciaccaluga',
+      title: 'Cloud Architect',
+      description: 'warren@coddi.ai',
       image: warren,
     },
     {
-      name: "Jourdan Bugueño Flores",
-      title: "Condition Monitoring Engineer",
-      description: "jourdan@coddi.ai",
+      name: 'Jourdan Bugueño Flores',
+      title: 'Condition Monitoring Engineer',
+      description: 'jourdan@coddi.ai',
       image: jourdan,
     },
     {
-      name: "Cristobal Perez Palma, MSC.",
-      title: "Data Science",
-      description: "cristobal@coddi.ai",
+      name: 'Cristobal Perez Palma, MSC.',
+      title: 'Data Science',
+      description: 'cristobal@coddi.ai',
       image: cristobal,
     },
     {
-      name: "Paola Arancibia Brito",
-      title: "CFO",
-      description: "paola@coddi.ai",
+      name: 'Paola Arancibia Brito',
+      title: 'CFO',
+      description: 'paola@coddi.ai',
       image: paola,
     },
     {
-      name: "Pedro Cancino Barraza",
-      title: "CEO",
-      description: "pedro@coddi.ai",
+      name: 'Pedro Cancino Barraza',
+      title: 'CEO',
+      description: 'pedro@coddi.ai',
       image: pedro,
     },
 
     // Add more team members here
-  ];
+  ]
 
   return (
     <section
-      id="theteam"
+      id='theteam'
       className={`theteam-section ${theme} ${
-        isVisible ? "fade-in" : "fade-out"
+        isVisible ? 'fade-in' : 'fade-out'
       }`}
       ref={sectionRef}
     >
-      <div className="left-column">
-        <h1>{language == "en" ? "Our Team" : "El Equipo"}</h1>
+      <div className='left-column'>
+        <h1>{language == 'en' ? 'Our Team' : 'El Equipo'}</h1>
       </div>
-      <div className="right-column">
-        <div className="right-grid">
+      <div className='right-column'>
+        <div className='right-grid'>
           {teamMembers.map((member, index) => (
-            <div className="team-member" key={index}>
+            <div className='team-member' key={index}>
               <img src={member.image} alt={`${member.name}`} />
               <h3>{member.name}</h3>
               <h4>{member.title}</h4>
@@ -84,7 +91,7 @@ const TheTeam = ({ theme, language }) => {
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default TheTeam;
+export default TheTeam
